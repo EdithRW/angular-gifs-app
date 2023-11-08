162753d8c2cf3d6dcaa5b81d5fe83481be0d5616
Edithss23 <edithss23@gmail.com>
2023-11-08 16:21:39 -0600

    end of gifs app base

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -13,7 +13,9 @@ export class GifsService {
   private apiKey : string = 'Cu0pPacxezqK9bAvWZ0PkARs1DVuSxPj';
   private serviceURL : string ='http://api.giphy.com/v1/gifs';
 
-  constructor(private http : HttpClient) { }
+  constructor(private http : HttpClient) {
+    this.loadLocalStorage();
+   }
 
   get tagsHistory(){
     return [...this._tagsHistory];
@@ -29,6 +31,23 @@ export class GifsService {
     this._tagsHistory.unshift(tag);
 
     this._tagsHistory = this._tagsHistory.splice(0,10);
+
+    this.saveLocalStorage();
+
+  }
+
+  private saveLocalStorage():void{
+    localStorage.setItem('history', JSON.stringify(this._tagsHistory));
+  }
+
+  private loadLocalStorage():void{
+    if(!localStorage.getItem('history'))return;
+
+    this._tagsHistory = JSON.parse(localStorage.getItem('history')!);
+
+    if(this._tagsHistory.length === 0)return;
+
+    this.searchTag(this._tagsHistory[0]);
 
   }
 
